adee9095f06e1d530ceba34c889dc1254ebff050
keka3avr <kekazavr228@gmail.com>
2021-06-20 22:36:32 +1100

    webpackGame initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path'); // Node.js модуль для разрешения путей файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                ],
+            },
+            {
+                test: /\.txt$/,
+                use: [
+                    'raw-loader',
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+        
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+
+};
